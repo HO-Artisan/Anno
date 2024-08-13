340c5a640fe9bc07100e72a1e6d150c330b79bf9
chemiofitor <98449295+chemiofitor@users.noreply.github.com>
2024-08-13 11:18:10 +0800

    fix

--- src/main/java/ho/artisan/anno/AnnoRegistration.java
+++ src/main/java/ho/artisan/anno/AnnoRegistration.java
@@ -2,9 +2,13 @@ package ho.artisan.anno;
 
 import ho.artisan.anno.core.annotation.ID;
 import ho.artisan.anno.core.annotation.datagen.Lang;
+import ho.artisan.anno.core.annotation.datagen.model.CubeAll;
 import ho.artisan.anno.core.annotation.datagen.model.Generated;
 import ho.artisan.anno.core.annotation.vanilla.Burnable;
 import ho.artisan.anno.core.annotation.vanilla.Reg;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 
 @ID("anno")
@@ -16,5 +20,12 @@ public class AnnoRegistration {
     @Reg("item")
     @ID("iron_coal")
     public static final Item IRON_COAL = new Item(new Item.Settings());
+    @CubeAll
+    @Lang(langCode = "en_us", value = "Iron Coal Block")
+    @Lang(langCode = "zh_cn", value = "铁煤炭块")
+    @Burnable(48000)
+    @Reg("block")
+    @ID("iron_coal_block")
+    public static final Block IRON_COAL_BLOCK = new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK));
 }
 
