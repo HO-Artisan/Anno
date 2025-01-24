2ed21ad6f6ab3a7c09aaf24aec2d32fb22a7546d
Chemiofitor4096 <hualongfentian123@outlook.com>
2025-01-24 17:47:22 +0800

    v3 rebuild

--- src/main/java/ho/artisan/anno/AnnoMod.java
+++ src/main/java/ho/artisan/anno/AnnoMod.java
@@ -12,7 +12,7 @@ public class AnnoMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        AnnoCore.INSTANCE.load();
+
     }
 
     public static Identifier id(String path) {
--- src/main/java/ho/artisan/anno/core/Anno.java
+++ src/main/java/ho/artisan/anno/core/Anno.java
@@ -1,107 +1,39 @@
 package ho.artisan.anno.core;
 
-import com.google.common.reflect.TypeToken;
-import ho.artisan.anno.AnnoUtil;
-import ho.artisan.anno.annotation.ID;
-import ho.artisan.anno.annotation.Priority;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.lang.annotation.Annotation;
-import java.lang.annotation.Repeatable;
 import java.lang.reflect.AnnotatedElement;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-/**
- * Anno is a class containing annotations, they will be saved as a list at initialization time.
- */
 public class Anno {
-    private final List<Annotation> annotations = new ArrayList<>();
+    private final Map<Class<? extends Annotation>, Annotation> map;
 
-    public Anno(AnnotatedElement element) {
-        Collections.addAll(annotations, element.getAnnotations());
+    protected Anno(AnnotatedElement element) {
+        map = Arrays.stream(element.getDeclaredAnnotations()).collect(
+                Collectors.toMap(Annotation::annotationType, Function.identity(), (first, second) -> first, LinkedHashMap::new)
+        );
     }
 
-    /**
-     * Adding other Anno's the designated annotation to this.
-     */
-    public <A extends Annotation> void add(Anno anno, Class<A> aClass) {
-        for (Annotation annotation : annotations) {
-            if (annotation.annotationType() != aClass)
-                annotations.add(anno.get(aClass));
-        }
-    }
-
-    /**
-     * A method which can get repeated annotation, it will return as a list.
-     */
-    @SuppressWarnings({"UnstableApiUsage", "unchecked"})
-    public <A extends Annotation> List<A> getRepeated(Class<A> aClass) {
-        Class<? extends Annotation> repeatable = new Anno(aClass).repeated();
-        if (repeatable == null) {
-            return List.of();
-        }
-        A[] a = (A[]) AnnoUtil.getValue(get(repeatable), new TypeToken<A[]>(){}.getRawType());
-        return a == null ? List.of() : List.of(a);
-    }
-
-    /**
-     * Whether this has the designated annotation.
-     */
-    public <A extends Annotation> boolean has(Class<A> aClass) {
-        return get(aClass) != null;
-    }
-
-    /**
-     * Get the designated annotation.
-     */
     public <A extends Annotation> A get(Class<A> aClass) {
-        for (Annotation annotation : annotations) {
-            if (annotation.annotationType() == aClass)
-                return aClass.cast(annotation);
-        }
-        return null;
+        return aClass.cast(map.get(aClass));
     }
 
-    /**
-     * Return this Anno`s id, which is located in {@link ID} annotation.
-     */
-    public String id() {
-        return get(ID.class).value();
+    public <A extends Annotation> boolean contain(Class<A> aClass) {
+        return aClass.isInstance(map.get(aClass));
     }
 
-    /**
-     * Return this Anno`s priority, which is located {@link Priority} annotation.
-     */
-    public int priority() {
-        Priority priority = get(Priority.class);
-        if (priority == null)
-            return 0;
-        return priority.value();
+    @ApiStatus.Experimental
+    public <A extends Annotation> void add(A annotation) {
+        map.put(annotation.annotationType(), annotation);
     }
 
-    /**
-     * Get this Anno`s {@link Repeatable} annotation if any.
-     */
-    @Nullable
-    public Class<? extends Annotation> repeated() {
-        Repeatable repeatable = get(Repeatable.class);
-        if (repeatable == null) {
-            return null;
-        }
-        return repeatable.value();
-    }
-
-    /**
-     * Get this Anno`s all annotations.
-     */
-    public List<Annotation> getAnnotations() {
-        return List.copyOf(annotations);
-    }
-
-    @Override
-    public String toString() {
-        return "Anno:" + id();
+    @ApiStatus.Experimental
+    public <A extends Annotation> void remove(Class<A> aClass) {
+        map.remove(aClass);
     }
 }
